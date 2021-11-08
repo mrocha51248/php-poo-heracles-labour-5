80313d1f1bf6b6112d627b3c6f14cd4e2d365612
Mario Rocha <mrocha51248@gmail.com>
2021-11-08 15:58:57 +0100

    Tile: add $x, $y and $image properties

--- src/Tile.php
+++ src/Tile.php
@@ -4,4 +4,79 @@ namespace App;
 
 class Tile
 {
+    private int $x;
+    private int $y;
+    private string $image;
+
+    /**
+     * Get the value of x
+     *
+     * @return int
+     */
+    public function getX(): int
+    {
+        return $this->x;
+    }
+
+    /**
+     * Set the value of x
+     *
+     * @param int $x
+     *
+     * @return self
+     */
+    public function setX(int $x): self
+    {
+        $this->x = $x;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of y
+     *
+     * @return int
+     */
+    public function getY(): int
+    {
+        return $this->y;
+    }
+
+    /**
+     * Set the value of y
+     *
+     * @param int $y
+     *
+     * @return self
+     */
+    public function setY(int $y): self
+    {
+        $this->y = $y;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of image
+     *
+     * @return string
+     */
+    public function getImage(): string
+    {
+        return $this->image;
+    }
+
+    /**
+     * Set the value of image
+     *
+     * @param string $image
+     *
+     * @return self
+     */
+    public function setImage(string $image): self
+    {
+        $this->image = $image;
+
+        return $this;
+    }
 }
