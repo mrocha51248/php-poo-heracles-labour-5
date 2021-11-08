c1574cb57431858c5869eb658a546a3b33a434cc
Mario Rocha <mrocha51248@gmail.com>
2021-11-08 16:05:24 +0100

    Mappable: declare interface methods

--- src/Mappable.php
+++ src/Mappable.php
@@ -4,4 +4,10 @@ namespace App;
 
 interface Mappable
 {
+    public function getX(): int;
+    public function setX(int $x): self;
+    public function getY(): int;
+    public function setY(int $y): self;
+    public function getImage(): string;
+    public function setImage(string $image): self;
 }
